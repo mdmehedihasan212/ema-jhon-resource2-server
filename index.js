3abed74c8f65b1f88e99bb1e40cbaef27d7fdcf4
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-23 22:32:42 +0600

    add mongodb connection and get api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,10 +9,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fpzhx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('emaJhon').collection('product');
+
+        // GET
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
 app.listen(port, () => {
-    console.log('Connection Ok');
+    console.log('Connection Ok', port);
 })
