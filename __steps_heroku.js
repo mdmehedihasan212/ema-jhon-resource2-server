ef6ff41021520bcb5096bb773ea36ed6798c5e14
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-27 21:09:00 +0600

    add heroku deploy steps

---
+++ __steps_heroku.js
@@ -0,0 +1,31 @@
+/**
+ * -----------------------
+ * ONE TIME your computer
+ * -----------------------
+ * Create heroku account
+ * Verify your email account
+ * Select your language and proceed
+ * Go their documentation
+ * Install heroku CLI
+ * heroku login
+ * -----------------
+ * FOR EACH PROJECT
+ * -----------------
+ * First of all check your server cmd
+ * heroku create
+ * git add . git commit -m"" git push
+ * git push heroku main
+ * Go heroku dashboard > Current Project > Settings > Reveal Config Vars
+ * Copy paste .env file for config key and value
+ * Make sure your whitelisted ip address to access mongodb
+ * ---------------
+ * UPDATE SERVER
+ * ---------------
+ * Make change
+ * git add . git commit -m"" git push
+ * git push heroku main
+ * ------------------------------
+ * CONNECT SERVER TO CLIENT SITE
+ * ------------------------------
+ *  Replace localhost:5000 to heroku deploy link
+ */
